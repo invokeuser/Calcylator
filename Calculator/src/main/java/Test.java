f0f392f4b6876d391613bbc3468d3b84e9c7314d
invokeuser <63235254+invokeuser@users.noreply.github.com>
2025-04-30 14:51:32 +0530

    Create Test.java

---
+++ Calculator/src/main/java/Test.java
@@ -0,0 +1,19 @@
+package ArraysTopic;
+
+public class loop1 {
+
+	public static void main(String[] args) {
+
+		for (int a = 0; a <= 3; a++) {
+			for (int k = 0; k <= 3; k++)
+			{
+				System.out.print(a+""+k+" ");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
+
