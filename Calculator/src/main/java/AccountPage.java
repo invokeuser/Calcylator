fec414adebf1fd50482aba1bd2d3b902b075c3e0
invokeuser <63235254+invokeuser@users.noreply.github.com>
2025-05-01 10:10:31 +0530

    Update AccountPage.java

--- Calculator/src/main/java/AccountPage.java
+++ Calculator/src/main/java/AccountPage.java
@@ -4,7 +4,7 @@ public class AccountPage {
 	public static void main(String[] args) {
 		int i = 10;
 		String name = "AccountPage";
-		System.out.println(AccountPage);
+		System.out.println(name);
 	}
 
 }
